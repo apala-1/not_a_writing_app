a828d8e2aa4a69ab704633e5ed5ab75460128338
apala-1 <apalalamichhane13579@gmail.com>
2026-01-27 11:22:37 +0545

    TASKS

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,12 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
 class ApiEndpoints {
   ApiEndpoints._();
 
-  // Base URL - change this for production
-  static const String baseUrl = 'http://10.0.2.2:3000/api/v1';
-  //static const String baseUrl = 'http://localhost:3000/api/v1';
-  // For Android Emulator use: 'http://10.0.2.2:3000/api/v1'
-  // For iOS Simulator use: 'http://localhost:5000/api/v1'
-  // For Physical Device use your computer's IP: 'http://192.168.x.x:5000/api/v1'
+  // Configuration
+  // static const bool isPhysicalDevice = false;
+  static const String _ipAddress = "10.63.123.66";
+  static const int _port = 3000;
+
+  // Base URLs
+  static String get _host {
+    if (Platform.isAndroid) {
+      return _ipAddress;
+    }
+    if (kIsWeb || Platform.isIOS) return 'localhost';
+    // if(Platform.isAndroid) return '10.0.2.2';
+    return 'localhost';
+  }
+
+  static String get serverUrl => 'http://$_host:$_port';
+  static String get baseUrl => '$serverUrl/api/v1';
+  static String get mediaServerUrl => serverUrl;
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
---
+++ lib/features/profile/presentation/pages/edit_profile_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class EditProfileScreen extends StatelessWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // pfp change 
+
+      // name change (only once every 30 mins)
+
+      // profession change
+
+      // bio change
+
+      // email change
+
+      // password change
+    );
+  }
+}
